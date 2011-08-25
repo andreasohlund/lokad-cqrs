29347d28d39a02b9bd21bb35de2afe8ceae4c0f4
Rinat Abdullin <rinat.abdullin@gmail.com>
2011-08-25 18:48:52 +0200

    Simplify protobuf serializer

--- Framework/Lokad.Cqrs.Azure/Core.Serialization/DataSerializerWithProtoBuf.cs
+++ Framework/Lokad.Cqrs.Azure/Core.Serialization/DataSerializerWithProtoBuf.cs
@@ -1,7 +1,8 @@
-﻿#region (c) 2010-2011 Lokad - CQRS for Windows Azure - New BSD License 
+﻿#region (c) 2010-2011 Lokad CQRS - New BSD License 
 
-// Copyright (c) Lokad 2010-2011, http://www.lokad.com
+// Copyright (c) Lokad SAS 2010-2011 (http://www.lokad.com)
 // This code is released as Open Source under the terms of the New BSD Licence
+// Homepage: http://lokad.github.com/lokad-cqrs/
 
 #endregion
 
@@ -13,84 +14,53 @@ using ProtoBuf.Meta;
 
 namespace Lokad.Cqrs.Core.Serialization
 {
-    public class DataSerializerWithProtoBuf : IDataSerializer
+    public class DataSerializerWithProtoBuf : AbstractDataSerializer
     {
-        readonly IDictionary<string, Type> _contract2Type = new Dictionary<string, Type>();
-        readonly IDictionary<Type, string> _type2Contract = new Dictionary<Type, string>();
         readonly IDictionary<Type, IFormatter> _type2Formatter = new Dictionary<Type, IFormatter>();
 
-        public DataSerializerWithProtoBuf(ICollection<Type> knownTypes)
+        public DataSerializerWithProtoBuf(ICollection<Type> knownTypes) : base(knownTypes)
         {
             if (knownTypes.Count == 0)
                 throw new InvalidOperationException(
                     "ProtoBuf requires some known types to serialize. Have you forgot to supply them?");
+            InitializeFormatters(knownTypes);
+        }
+
+        void InitializeFormatters(IEnumerable<Type> knownTypes)
+        {
             foreach (var type in knownTypes)
             {
-                var reference = ContractEvil.GetContractReference(type);
                 var formatter = RuntimeTypeModel.Default.CreateFormatter(type);
-
-                try
-                {
-                    _contract2Type.Add(reference, type);
-                }
-                catch(ArgumentException e)
-                {
-                    var msg = string.Format("Duplicate contract '{0}' being added to ProtoBuf dictionary", reference);
-                    throw new InvalidOperationException(msg, e);
-                }
-                try
-                {
-                    _type2Contract.Add(type, reference);
-                    _type2Formatter.Add(type, formatter);
-                }
-                catch (ArgumentException e)
-                {
-                    var msg = string.Format("Duplicate type '{0}' being added to ProtoBuf dictionary", type);
-                    throw new InvalidOperationException(msg, e);
-                }
+                _type2Formatter.Add(type, formatter);
             }
         }
 
-        public void Serialize(object instance, Stream destination)
+        public override void Serialize(object instance, Stream destination)
         {
             IFormatter formatter;
             if (!_type2Formatter.TryGetValue(instance.GetType(), out formatter))
             {
-                var s = string.Format("Can't find serializer for unknown object type '{0}'. Have you passed all known types to the constructor?", instance.GetType());
+                var s =
+                    string.Format(
+                        "Can't find serializer for unknown object type '{0}'. Have you passed all known types to the constructor?",
+                        instance.GetType());
                 throw new InvalidOperationException(s);
             }
             formatter.Serialize(destination, instance);
         }
 
-        public object Deserialize(Stream source, Type type)
+        public override object Deserialize(Stream source, Type type)
         {
             IFormatter value;
             if (!_type2Formatter.TryGetValue(type, out value))
             {
-                var s = string.Format("Can't find serializer for unknown object type '{0}'. Have you passed all known types to the constructor?", type);
+                var s =
+                    string.Format(
+                        "Can't find serializer for unknown object type '{0}'. Have you passed all known types to the constructor?",
+                        type);
                 throw new InvalidOperationException(s);
             }
             return value.Deserialize(source);
-        }
-
-        /// <summary>
-        /// Gets the contract name by the type
-        /// </summary>
-        /// <param name="messageType">Type of the message.</param>
-        /// <returns>contract name (if found)</returns>
-        public bool TryGetContractNameByType(Type messageType, out string contractName)
-        {
-            return _type2Contract.TryGetValue(messageType, out contractName);
-        }
-
-        /// <summary>
-        /// Gets the type by contract name.
-        /// </summary>
-        /// <param name="contractName">Name of the contract.</param>
-        /// <returns>type that could be used for contract deserialization (if found)</returns>
-        public bool TryGetContractTypeByName(string contractName, out Type contractType)
-        {
-            return _contract2Type.TryGetValue(contractName, out contractType);
         }
     }
 }
