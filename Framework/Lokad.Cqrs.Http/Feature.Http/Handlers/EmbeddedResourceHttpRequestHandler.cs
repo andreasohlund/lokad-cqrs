2a7d9b20e8976ebf5921761555e95c89b77f54a7
Alexandr Zaozerskiy <alexandr.zaozerskiy@lokad.com>
2011-08-31 15:52:25 +0600

    Response Content-Type header handling.

--- Framework/Lokad.Cqrs.Http/Feature.Http/Handlers/EmbeddedResourceHttpRequestHandler.cs
+++ Framework/Lokad.Cqrs.Http/Feature.Http/Handlers/EmbeddedResourceHttpRequestHandler.cs
@@ -26,35 +26,41 @@ namespace Lokad.Cqrs.Feature.Http.Handlers
 
             foreach (var f in filter)
             {
-                _set.Add("/" + f.Remove(0, ns.Length), f);
+                _set.Add("." + f.Remove(0, ns.Length), f);
             }
         }
 
 
         public bool WillHandle(IHttpContext context)
         {
-            return _set.ContainsKey(context.GetRequestUrl());
+            return _set.ContainsKey((context.GetRequestUrl()??"").Replace('/','.'));
         }
 
         public void Handle(IHttpContext context)
         {
-            var requestUrl = context.GetRequestUrl();
+            var requestUrl = (context.GetRequestUrl() ?? "").Replace('/', '.');
             var resource = _set[requestUrl];
+            
+            GuessContentType(resource).IfValue(s => context.Response.ContentType = s);
             using (var r = _resourceAssembly.GetManifestResourceStream(resource))
             {
                 
                 r.CopyTo(context.Response.OutputStream);
             }
-            GuessContentType(resource).IfValue(s => context.Response.ContentType = s);
         }
 
         static Optional<string> GuessContentType(string fileName)
         {
             if (string.IsNullOrEmpty(fileName))
                 return Optional<string>.Empty;
+
             var path = Path.GetExtension(fileName).ToLowerInvariant();
             switch (path)
             {
+                case ".htm":
+                    return "text/html";
+                case ".js":
+                    return "text/javascript";
                 default:
                     return Optional<string>.Empty;
             }
