4543397c38cce2f61725573952ca23f160a88eb1
Rinat Abdullin <rinat.abdullin@gmail.com>
2011-09-01 09:01:18 +0200

    Autofac provider registers autofac in Funq

--- Framework/Lokad.Cqrs.Autofac/Feature.HandlerClasses/AutofacContainerProvider.cs
+++ Framework/Lokad.Cqrs.Autofac/Feature.HandlerClasses/AutofacContainerProvider.cs
@@ -1,3 +1,11 @@
+#region (c) 2010-2011 Lokad CQRS - New BSD License 
+
+// Copyright (c) Lokad SAS 2010-2011 (http://www.lokad.com)
+// This code is released as Open Source under the terms of the New BSD Licence
+// Homepage: http://lokad.github.com/lokad-cqrs/
+
+#endregion
+
 using System;
 using Autofac;
 using Lokad.Cqrs.Core;
@@ -12,7 +20,7 @@ namespace Lokad.Cqrs.Feature.HandlerClasses
         public static IContainerForHandlerClasses Build(Container container, Type[] handlerTypes)
         {
             var autofacBuilder = new ContainerBuilder();
-            
+
             foreach (var handlerType in handlerTypes)
             {
                 autofacBuilder.RegisterType(handlerType);
@@ -21,6 +29,7 @@ namespace Lokad.Cqrs.Feature.HandlerClasses
             autofacBuilder.RegisterSource(new FunqAdapterForAutofac(container));
 
             var autofacContainer = autofacBuilder.Build();
+            container.Register(autofacContainer);
             return new AutofacContainerForHandlerClasses(autofacContainer);
         }
     }
