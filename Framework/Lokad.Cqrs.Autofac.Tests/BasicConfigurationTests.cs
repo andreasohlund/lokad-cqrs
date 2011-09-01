41f616b4437004f39a29db7ac4999addab2b17da
Vsevolod Parfenov <vsevolodp@gmail.com>
2011-09-01 13:34:21 +0600

    Add failing test.

---
+++ Framework/Lokad.Cqrs.Autofac.Tests/BasicConfigurationTests.cs
@@ -0,0 +1,19 @@
+﻿using Lokad.Cqrs.Build.Engine;
+using NUnit.Framework;
+
+namespace Lokad.Cqrs
+{
+    [TestFixture]
+    public class BasicConfigurationTests
+    {
+        [Test]
+        public void CanBeConfiguredWithHandlerSample()
+        {
+            var builder = new CqrsEngineBuilder();
+            builder.MessagesWithHandlersFromAutofac(
+                c => c.HandlerSample<Define.Handle<Define.Command>>(h => h.Handle(null)));
+
+            using (builder.Build()) {}
+        }
+    }
+}
